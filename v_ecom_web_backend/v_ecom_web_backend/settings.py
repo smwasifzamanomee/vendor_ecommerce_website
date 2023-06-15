90fb3a4eead327d16557a8004e1742e15fd0b167
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-06-15 08:37:01 +0600

    project database postgres setup

--- v_ecom_web_backend/v_ecom_web_backend/settings.py
+++ v_ecom_web_backend/v_ecom_web_backend/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'v_ecom_web_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # postgresql
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'v_ecom_db',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST' : 'localhost',
     }
 }
 
