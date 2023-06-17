2cd4b3f2ea84f3e1720e1d53f08e9d5e22cd8a66
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-06-17 20:41:57 +0600

    project and tailwind setup

--- v_ecom_web_frontend/pages/index.js
+++ v_ecom_web_frontend/pages/index.js
@@ -1,4 +1,4 @@
-export default function App() {
+export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
--- v_ecom_web_frontend/tailwind.config.js
+++ v_ecom_web_frontend/tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Add our colors
+        primary: "#808080",
+        secondary: "#FFFFFF",
+      },
+      screens: {
+        //Add our screen sizes
+        'sm': {'min': '370px','max': '768px'},
+        'md': {'min': '769px', 'max': '992px'},
+        'lg': {'min': '993px' },
+        'xl': { 'min': '1300px'}
+      }
+    },
   },
   plugins: [],
 }
-
