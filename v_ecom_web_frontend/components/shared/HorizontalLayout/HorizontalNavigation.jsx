b3e1c0592a43d5dfe84ae2fa892837383388693d
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-06-22 20:31:02 +0600

    my account register

--- v_ecom_web_frontend/components/shared/HorizontalLayout/HorizontalNavigation.jsx
+++ v_ecom_web_frontend/components/shared/HorizontalLayout/HorizontalNavigation.jsx
@@ -7,11 +7,18 @@ import Button from "@/components/utils/Button";
 import CustomLink from "@/components/utils/CustomLink";
 import logo from "@/public/images/Vendor.png";
 import { HiShoppingCart } from "react-icons/hi";
+import { useState } from "react";
 
 // Horizontal Navigation Component
 const index = () => {
   const router = useRouter();
   const { pathname } = router;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className=" bg-secondary ">
@@ -57,14 +64,65 @@ const index = () => {
               className="flex items-center justify-center gap-x-2 px-4 rounded-full text-white hover:text-primary"
               href={`tel:+8801910312566`}
             >
-              <FaPhoneAlt/> <span>+8801910312566</span></a>
+              <FaPhoneAlt /> <span>+8801910312566</span></a>
 
-            <Link href="/registration">
-              <Button width={"xl:w-[150px] lg:w-[100px]"} bgColor={"bg-transparent hover:bg-primary"} className={
-                "border border-primary text-primary hover:text-white"
-              } > Sign Up
-              </Button>
-            </Link>
+            {/* dropdown  */}
+            <div className="relative inline-block text-left">
+              <div>
+                <button
+                  type="button"
+                  className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  id="menu-button"
+                  aria-expanded={isOpen}
+                  aria-haspopup="true"
+                  onClick={toggleMenu}
+                >
+                  Account
+                  <svg
+                    className={`-mr-1 h-5 w-5 text-gray-400 ${isOpen ? 'transform rotate-180' : ''}`}
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+              {isOpen && (
+                <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-tertiary shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                  <div className="py-1" role="none">
+                    <Link href="/register">
+                      <p className="text-white block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">
+                        Register
+                      </p>
+                    </Link>
+                    <p className="text-white block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">
+                      Login
+                    </p>
+                    <p className="text-white block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-2">
+                      Dashboard
+                    </p>
+                    <form method="POST" action="#" role="none">
+                      <button
+                        type="submit"
+                        className="text-white block w-full px-4 py-2 text-left text-sm"
+                        role="menuitem"
+                        tabIndex="-1"
+                        id="menu-item-3"
+                      >
+                        Logout
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            {/* end */}
           </>
         </div>
       </nav>
